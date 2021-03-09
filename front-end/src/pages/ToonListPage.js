e321bf6bc6138bf26d133f7dba6b944660d42840
Medhat Elmasry <medhat@elmasry.ca>
2021-03-09 11:34:38 -0800

    added react template

---
+++ front-end/src/pages/ToonListPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ToonList from '../components/ToonList';
+
+const ToonListPage = () => (
+    <>
+        <h1>Toons List Page</h1>
+           
+         <ToonList />
+    </>
+);
+export default ToonListPage;
