6173dc507d837b25306fab045acb302d193d3e89
oves98 <oves.patel98@gmail.com>
2021-03-22 21:46:41 -0700

    made the landing pages

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project Maestro</title>
+</head>
+<body>
+    <h1> Welcome to Project Maestro </h1>
+  
+    <a href="./back-end/index.php"> Click to go to Back End Page </a>
+</body>
+</html>
