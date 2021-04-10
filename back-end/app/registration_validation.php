d6e473c2ad8489645dbaaa38652c6cfd19225883
Helen <howareyouhelen@hotmail.com>
2021-04-10 16:23:25 -0700

    fix on registration validation for instructor existing email check.

--- back-end/app/registration_validation.php
+++ back-end/app/registration_validation.php
@@ -18,43 +18,48 @@ if (isset($_POST['register'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    $check_duplicate_email = $conn->query('SELECT * FROM User WHERE UserEmail == $email');
-    if($check_duplicate_email){
-        $_SESSION['dup_error'] = 'User with this email already exists';
-        header('Location: registration.php');
-    } else {
+    $check_duplicate_email = "SELECT COUNT(*) FROM User WHERE UserEmail = ?";
+    
+    $stmt1 = $conn->prepare($check_duplicate_email);
+    $stmt1->bindParam(1, $email);
+    $result = $stmt1->execute();
+    while ($row = $result->fetchArray()) {
+        if($row[0] < 1) {
+            $query = "INSERT INTO $table_name";
+            $query .= " (UserEmail,UserFName,UserLName,UserPassword)";
+            $query .= " VALUES";
+            $query .= " (:email,:firstName,:lastName,:password)";
 
-        $query = "INSERT INTO $table_name";
-        $query .= " (UserEmail,UserFName,UserLName,UserPassword)";
-        $query .= " VALUES";
-        $query .= " (:email,:firstName,:lastName,:password)";
+            $stmt = $conn->prepare($query);
 
-        $stmt = $conn->prepare($query);
+            $stmt->bindParam(':firstName', $firstName);
+            $stmt->bindParam(':lastName', $lastName);
+            $stmt->bindParam(':email', $email);
 
-        $stmt->bindParam(':firstName', $firstName);
-        $stmt->bindParam(':lastName', $lastName);
-        $stmt->bindParam(':email', $email);
+            $password_hash = password_hash($password, PASSWORD_BCRYPT);
 
-        $password_hash = password_hash($password, PASSWORD_BCRYPT);
+            $stmt->bindParam(':password', $password_hash);
+            $query_results = $stmt->execute();
 
-        $stmt->bindParam(':password', $password_hash);
-        $query_results = $stmt->execute();
+            if ($query_results) {
+                $id_query = $conn->query('SELECT UserId FROM User ORDER BY UserId DESC LIMIT 0,1');
+                while ($row = $id_query->fetchArray()) {
+                    $id = $row['UserId'];
+                }
+                $query_results = NULL;  // closes connection
+                $id_query = NULL;  // closes connection
+                http_response_code(200);
+                $_SESSION['success'] = 'Successfully registered a user';
+                $_SESSION['instructor_id'] = $id;
+                header('Location: home.php');
+            } else {
+                http_response_code(400);
 
-        if ($query_results) {
-            $id_query = $conn->query('SELECT UserId FROM User ORDER BY UserId DESC LIMIT 0,1');
-            while ($row = $id_query->fetchArray()) {
-                $id = $row['UserId'];
+                echo json_encode(array("message" => "Unable to register the user."));
             }
-            $query_results = NULL;  // closes connection
-            $id_query = NULL;  // closes connection
-            http_response_code(200);
-            $_SESSION['success'] = 'Successfully registered a user';
-            $_SESSION['instructor_id'] = $id;
-            header('Location: home.php');
         } else {
-            http_response_code(400);
-
-            echo json_encode(array("message" => "Unable to register the user."));
+            $_SESSION['dup_error'] = 'User with this email already exists';
+            header('Location: registration.php');
         }
     }
 }
