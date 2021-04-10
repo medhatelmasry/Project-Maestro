c1ea1eec85a3338d7832816cd8b1ede73c554b11
William He <contact@williamhe.dev>
2021-04-10 10:17:32 -0700

    'View Projects' and view_projects.php works properly now

--- back-end/app/view_projects.php
+++ back-end/app/view_projects.php
@@ -15,10 +15,33 @@
 	<div class="container">
         <h3>Projects</h3>
         <table width='100%' class='table table-striped'>
-            <tr>
+            <!-- <tr>
                 <td>Team 1</td>
                 <td><button type="button" class="btn btn-info" onclick="window.location.href='./view_project.php'">View Project</button></td>
-            </tr>
+            </tr> -->
+			<?php
+				include_once '../db/inc_db_helper.php';
+
+				if (isset($_GET['id'])) {
+					$db_helper = new DatabaseHelper('../db/projectmaestro.db');
+					$conn = $db_helper->getConn();
+					
+					$courseId = $GET['id'];
+
+					$res = $db_helper->getData("Project", "ProjectId", $courseId);
+					echo "<tr><th>Project ID</th>" .
+						"<th>Project Name</th>" .
+						"<th>Course ID</th></tr>";
+					while ($row = $res->fetchArray()) {
+						echo "<tr><td>{$row['ProjectId']}</td>" .
+							"<td>{$row['ProjectName']}</td>" .
+							"<td>{$row['CourseId']}</td>";
+						echo "<td>";
+						echo "<a class='btn btn-small btn-success' href='./view_project.php?id={$row['ProjectId']}'>View</a>";
+						echo "</td></tr>\n";
+				}
+				}
+			?>
         </table>
 	</div>
 </body>
