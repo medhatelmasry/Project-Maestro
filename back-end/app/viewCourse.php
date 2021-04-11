ab6744e042bcd994cf58184569fb3535f9325956
oves98 <oves.patel98@gmail.com>
2021-04-11 12:57:20 -0700

    added a validation so the instructor can only see thier own course

--- back-end/app/viewCourse.php
+++ back-end/app/viewCourse.php
@@ -43,7 +43,7 @@ $connection = $db->getConn();
 
 $id = $_SESSION['instructor_id'];
 			
-$check_id = "SELECT * FROM Course WHERE InstructorId = $id";
+$check_id = "SELECT * FROM Course WHERE UserId = $id";
 
 $res = $connection->query($check_id);
         echo "<table width='100%' class='table table-striped'>\n";
