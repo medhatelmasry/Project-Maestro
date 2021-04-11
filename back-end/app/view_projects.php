6395bcda360f9e90f0a5a108d746be6757b92356
Chris Khoan <cuongkhoan2312@gmail.com>
2021-04-11 14:19:32 -0700

    passed courseId to view_project.php

--- back-end/app/view_projects.php
+++ back-end/app/view_projects.php
@@ -32,7 +32,7 @@
 			<?php
 				$res = $db->getData("Project", "ProjectOutlineId", $ProjectOutlineId);
 				while ($row = $res->fetchArray()) {
-					$view_project = "./view_project.php?projectId={$row['ProjectId']}";
+					$view_project = "./view_project.php?projectId={$row['ProjectId']}&courseId={$CourseId}";
 					echo "<tr><td>{$row['ProjectName']}</td>".
 					"<td class='alignRight'>".
 					"<a class='btn btn-small btn-primary' href='".$view_project."'>View Project</a>".
