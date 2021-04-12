0c3e3ce5fa7829131b4665cc286b899e4a9aa84f
mikehuang <45245414+mikehuang123@users.noreply.github.com>
2021-04-12 15:32:39 -0700

    styling
    add some styling -> <div class = container></div>

--- back-end/app/createCourse.php
+++ back-end/app/createCourse.php
@@ -12,68 +12,73 @@
             <a class="navbar-brand navbar-right" href="logout.php">Logout</a>
 		</div>
 </nav>
-<?php 
-session_start();
-?>
+<div class = "container">
+    <?php 
+    session_start();
+    ?>
 
+        
+    <?php
+    include ('../db/inc_db_helper.php');
+
+    $db = new DatabaseHelper('../db/projectmaestro.db');
+    ?>
+
+
+    <a href="viewCourse.php" class="btn btn-small btn-success">View Courses</a>
+    <h1>Create Course</h1>
+    <?php
+    //checking if the session 'dup_course' is set. Dup_course session is the message if the course already exists in the database.
+    if(ISSET($_SESSION['dup_course'])){
+    ?>
+            <div id="error" class="alert alert-danger"><?php echo $_SESSION['dup_course']?></div>
+            <?php
+            //Unsetting the 'dup_course' session after displaying the message.
+            unset($_SESSION['dup_course']);
+    }
+    ?> 
     
-<?php
-include ('../db/inc_db_helper.php');
-
-$db = new DatabaseHelper('../db/projectmaestro.db');
-?>
-
-
-
-<h1>Create Course</h1>
-<?php
-//checking if the session 'dup_course' is set. Dup_course session is the message if the course already exists in the database.
-if(ISSET($_SESSION['dup_course'])){
-?>
-        <div id="error" class="alert alert-danger"><?php echo $_SESSION['dup_course']?></div>
-        <?php
-        //Unsetting the 'dup_course' session after displaying the message.
-        unset($_SESSION['dup_course']);
-}
-?> 
-<div class="row">
-    <div class="col-md-4">
-        <form action="processCreate.php" method="post">
-
-        <div class="form-group">
-                <label for="InstructorID" class="control-label">Instructor ID: <?php echo $_SESSION['instructor_id'] ?></label>                
-            </div>
+    <div class="row">
+        <div class="col-md-4">
+            <form action="processCreate.php" method="post">
+        
             <div class="form-group">
+                    <label for="InstructorID" class="control-label">Instructor ID: <?php echo $_SESSION['instructor_id'] ?></label>                
+                </div>
+            
 
-                <label for="CourseID" class="control-label">Course ID</label>
-                <input for="CourseID" class="form-control" name="CourseID" id="CourseID" required="required" />
-            </div>
+                <div class="form-group">
 
-            <div class="form-group">
-                <label for="CourseName" class="control-label">Course Name</label>
-                <input for="CourseName" class="form-control" name="CourseName" id="CourseName" required="required"  />
-            </div>
+                    <label for="CourseID" class="control-label">Course ID</label>
+                    <input for="CourseID" class="form-control" name="CourseID" id="CourseID" required="required" />
+                </div>
 
-            <div class="form-group">
-                <label for="CourseTerm" class="control-label">Course Term</label>
-                <input for="CourseTerm" class="form-control" name="CourseTerm" id="CourseTerm" required="required" />
-            </div>
+                <div class="form-group">
+                    <label for="CourseName" class="control-label">Course Name</label>
+                    <input for="CourseName" class="form-control" name="CourseName" id="CourseName" required="required"  />
+                </div>
 
-            <div class="form-group">
-            <input type="submit" value="Create" name="create" class="btn btn-small btn-success" />  
-            </div>
+                <div class="form-group">
+                    <label for="CourseTerm" class="control-label">Course Term</label>
+                    <input for="CourseTerm" class="form-control" name="CourseTerm" id="CourseTerm" required="required" />
+                </div>
 
-        </form> 
+                <div class="form-group">
+                <a href="home.php" class="btn btn-small btn-success">Back</a>
+                <input type="submit" value="Create" name="create" class="btn btn-small btn-success" />  
+                </div>
 
-     
- 
+            </form> 
+
+        
+    
+        </div>
+    
+
+
     </div>
-   
-
+            
+        
 
 </div>
-        <a href="home.php" class="btn btn-small btn-success">Back</a>
-        <a href="viewCourse.php" class="btn btn-small btn-success">View Courses</a>
-
-
 
