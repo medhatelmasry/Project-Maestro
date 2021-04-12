0ccd37e26297d2b0eadcba111e9d8b67b93ae815
oves98 <oves.patel98@gmail.com>
2021-04-12 13:43:30 -0700

    added headers to create course pages

--- back-end/app/createCourse.php
+++ back-end/app/createCourse.php
@@ -1,23 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1"/>
+		<!-- Bootstrap -->
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
+		<!-- <link rel="stylesheet" type="text/css" href="css/backend_style.css"/> -->
+	</head>
+<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="home.php">Project Maestro</a>
+            <a class="navbar-brand navbar-right" href="logout.php">Logout</a>
+		</div>
+</nav>
 <?php 
 session_start();
 ?>
-<link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
+
     
 <?php
 include ('../db/inc_db_helper.php');
 
 $db = new DatabaseHelper('../db/projectmaestro.db');
-
 ?>
 
 
-<nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<a class="navbar-brand" href="home.php">Project Maestro</a>
-            <a class="navbar-brand navbar-right" href="logout.php">Logout</a>
-
-		</div>
-</nav>
 
 <h1>Create Course</h1>
 <?php
--- back-end/app/viewCourse.php
+++ back-end/app/viewCourse.php
@@ -1,12 +1,16 @@
 <?php session_start(); ?>
-
-<link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
-
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1"/>
+		<!-- Bootstrap -->
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
+		<!-- <link rel="stylesheet" type="text/css" href="css/backend_style.css"/> -->
+	</head>
 <nav class="navbar navbar-default">
 		<div class="container-fluid">
 			<a class="navbar-brand" href="home.php">Project Maestro</a>
             <a class="navbar-brand navbar-right" href="logout.php">Logout</a>
-
 		</div>
 </nav>
 
